0082ba81447d67794cb9428df5a45e1773ff5736
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-20 17:51:42 +0100

    start with repo

---
+++ main.js
@@ -0,0 +1,15 @@
+function openMe () {
+  document.getElementById('navbar').style.height = '100%'
+}
+function closeMe () {
+  document.getElementById('navbar').style.height = '0%'
+}
+
+// function hideShow () {
+//   let hiddenElement = document.getElementById('hidden')
+//   if (hiddenElement.style.display === 'none') {
+//     hiddenElement.style.display = 'block'
+//   } else {
+//     hiddenElement.style.display = 'none'
+//   }
+// }
