0b399bef08b7ce926e4f6981bab997435227299e
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-23 23:12:31 +0100

    fixed partner section

--- dynamic.js
+++ dynamic.js
@@ -66,32 +66,31 @@ const featuredSpeakers3 = [
     img2: 'img/speaker1.jpg'
   }
 ]
-
 const speakers1 = document.querySelector('.columns-row1')
 
 for (let i = 0; i < featuredSpeakers1.length; i += 1) {
-  const featuredSpeaker1 = featuredSpeakers1[i]
+   const featuredSpeaker1 = featuredSpeakers1[i]
 
-  speakers1.innerHTML += `
-       
-       <div  id='${featuredSpeaker1.id}' class='card-wraper col-xl-4 align-items-center py-3 mb-5'>
+   speakers1.innerHTML += `
+        
+      <div  id='${featuredSpeaker1.id}' class='card-wraper col-md-4 col-lg-4 col-xl-4  align-items-center py-3 mb-5'>
        <div class="row ">
-        <div class="images col-4">
-            <img class="image1 img-fluid" src="${featuredSpeaker1.img1}" alt="" width=70% height=80%>
-            <img class="image2  " src="${featuredSpeaker1.img2}" alt='' width=80% height=150%>
+         <div class="images col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+              <img class="image1 img-fluid" src="${featuredSpeaker1.img1}" alt="" width=70% height=80%>
+             <img class="image2  " src="${featuredSpeaker1.img2}" alt='' width=80% height=150%>
    
+         </div>
+         <div class="description col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
+             <h4>${featuredSpeaker1.name}</h4>
+             <h5 class='red fst-italic fw-bold'>${featuredSpeaker1.description}
+             </h5>
+             <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker1.about}</h6>
+             <span class='line2'></span>
+         </div>
         </div>
-        <div class="description col-8 no-gutters">
-            <h4>${featuredSpeaker1.name}</h4>
-            <h5 class='red fst-italic fw-bold'>${featuredSpeaker1.description}
-            </h5>
-            <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker1.about}</h6>
-            <span class='line2'></span>
-        </div>
-       </div>
-        </div>               
-  </div>
-  `;
+         </div>               
+   </div>
+   `;
 }
 
  const speakers2 = document.querySelector('.columns-row2')
@@ -101,13 +100,14 @@ for (let i = 0; i < featuredSpeakers1.length; i += 1) {
 
    speakers2.innerHTML += `
         
-      <div  id='${featuredSpeaker2.id}' class='card-wraper col-xl-4 align-items-center py-3 mb-5'>
+      <div  id='${featuredSpeaker2.id}' class='card-wraper col-md-4 col-lg-4 col-xl-4  align-items-center py-3 mb-5'>
        <div class="row ">
-         <div class="images col-4">             <img class="image1 img-fluid" src="${featuredSpeaker2.img1}" alt="" width=70% height=80%>
+         <div class="images col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+              <img class="image1 img-fluid" src="${featuredSpeaker2.img1}" alt="" width=70% height=80%>
              <img class="image2  " src="${featuredSpeaker2.img2}" alt='' width=80% height=150%>
    
          </div>
-         <div class="description col-8 no-gutters">
+         <div class="description col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
              <h4>${featuredSpeaker2.name}</h4>
              <h5 class='red fst-italic fw-bold'>${featuredSpeaker2.description}
              </h5>
@@ -127,14 +127,14 @@ for (let i = 0; i < featuredSpeakers1.length; i += 1) {
 
    speakers3.innerHTML += `
        
-        <div  id='${featuredSpeaker3.id}' class='card-wraper col-xl-4 align-items-center py-3 mb-5'>
+        <div  id='${featuredSpeaker3.id}' class='card-wraper  col-md-4 col-lg-4 col-xl-4 align-items-center py-3 mb-5'>
         <div class="row ">
-         <div class="images col-4">
+         <div class="images col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
             <img class="image1 img-fluid" src="${featuredSpeaker3.img1}" alt="" width=70% height=80%>
             <img class="image2  " src="${featuredSpeaker3.img2}" alt='' width=80% height=150%>
    
         </div>
-        <div class="description col-8 no-gutters">
+        <div class="description col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
             <h4>${featuredSpeaker3.name}</h4>
             <h5 class='red fst-italic fw-bold'>${featuredSpeaker3.description}
             </h5>
