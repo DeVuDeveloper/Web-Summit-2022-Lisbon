1c0e7f579273cace579e27dc9e55d91906933697
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-23 14:51:44 +0100

    logo fix

--- main.js
+++ main.js
@@ -11,9 +11,14 @@ function closeMe () {
   let hiddenElement = document.querySelectorAll('#hide-cards');
   hiddenElement.forEach((item) =>{
     if(item.style.visibility === 'hidden') {
-     item.style.visibility = 'visible'
+     item.style.visibility = 'visible';
+     item.style.height = '100%';
+     document.querySelector('.hide-btn a').textContent='LESS';
+
     } else {
-      item.style.visibility = 'hidden'
+      item.style.visibility = 'hidden';
+      item.style.height = '0';
+      document.querySelector('.hide-btn a').textContent='MORE';
     }
   });
 }
