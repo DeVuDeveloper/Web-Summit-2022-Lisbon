41ca0cb6926648d422678046a1cde508e4ba9dab
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-24 20:01:51 +0100

    fixed event Listeners instead function in js

--- dynamic.js
+++ dynamic.js
@@ -74,7 +74,7 @@ for (let i = 0; i < featuredSpeakers1.length; i += 1) {
              <img class="image2 " src="${featuredSpeaker1.img2}" alt='' width=80% height=150%>
    
          </div>
-         <div class="description1 col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
+         <div class="description1 col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 no-gutters">
              <h4>${featuredSpeaker1.name}</h4>
              <h5 class='red fst-italic fw-bold'>${featuredSpeaker1.description}
              </h5>
@@ -89,54 +89,56 @@ for (let i = 0; i < featuredSpeakers1.length; i += 1) {
 
 const speakers2 = document.querySelector('.columns-row2');
 
-for (let i = 0; i < featuredSpeakers1.length; i += 1) {
+for (let i = 0; i < featuredSpeakers2.length; i += 1) {
   const featuredSpeaker2 = featuredSpeakers2[i];
 
   speakers2.innerHTML += `
         
-      <div  id='${featuredSpeaker2.id}' class='card-wraper col-md-4 col-lg-4 col-xl-4  align-items-center '>
-       <div class="row ">
-         <div class="images col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-              <img class="image1 img-fluid" src="${featuredSpeaker2.img1}" alt="" width=70% height=80%>
-             <img class="image2  " src="${featuredSpeaker2.img2}" alt='' width=80% height=150%>
-   
-         </div>
-         <div class="description col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
-             <h4>${featuredSpeaker2.name}</h4>
-             <h5 class='red fst-italic fw-bold'>${featuredSpeaker2.description}
-             </h5>
-             <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker2.about}</h6>
-             <span class='line2'></span>
-         </div>
-        </div>
-         </div>               
+        
+  <div  id='${featuredSpeaker2.id}' class='card-wraper1 col-md-4 col-lg-4 col-xl-4  align-items-center mb-3'>
+  <div class="row ">
+    <div class="images1  col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+         <img class="image1 img-fluid" src="${featuredSpeaker2.img1}" alt="" width=70% height=80%>
+        <img class="image2 " src="${featuredSpeaker2.img2}" alt='' width=80% height=150%>
+
+    </div>
+    <div class="description1 col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 no-gutters">
+        <h4>${featuredSpeaker2.name}</h4>
+        <h5 class='red fst-italic fw-bold'>${featuredSpeaker2.description}
+        </h5>
+        <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker2.about}</h6>
+        <span class='line2'></span>
+    </div>
    </div>
-   `;
+    </div>               
+</div>
+`;
 }
 
 const speakers3 = document.querySelector('.columns-row3');
 
-for (let i = 0; i < featuredSpeakers1.length; i += 1) {
+for (let i = 0; i < featuredSpeakers3.length; i += 1) {
   const featuredSpeaker3 = featuredSpeakers3[i];
 
   speakers3.innerHTML += `
-       
-        <div  id='${featuredSpeaker3.id}' class='card-wraper  col-md-4 col-lg-4 col-xl-4 align-items-center '>
-        <div class="row ">
-         <div class="images col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-            <img class="image1 img-fluid" src="${featuredSpeaker3.img1}" alt="" width=70% height=80%>
-            <img class="image2  " src="${featuredSpeaker3.img2}" alt='' width=80% height=150%>
-   
-        </div>
-        <div class="description col-8 col-sm-4 col-md-8 col-lg-8 col-xl-8 no-gutters">
-            <h4>${featuredSpeaker3.name}</h4>
-            <h5 class='red fst-italic fw-bold'>${featuredSpeaker3.description}
-            </h5>
-            <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker3.about}</h6>
-             <span class='line2'></span>
-         </div>
-        </div>
-        </div>               
-  </div>
-   `;
+        
+        
+  <div  id='${featuredSpeaker3.id}' class='card-wraper1 col-md-4 col-lg-4 col-xl-4  align-items-center mb-3'>
+  <div class="row ">
+    <div class="images1  col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+         <img class="image1 img-fluid" src="${featuredSpeaker3.img1}" alt="" width=70% height=80%>
+        <img class="image2 " src="${featuredSpeaker3.img2}" alt='' width=80% height=150%>
+
+    </div>
+    <div class="description1 col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 no-gutters">
+        <h4>${featuredSpeaker3.name}</h4>
+        <h5 class='red fst-italic fw-bold'>${featuredSpeaker3.description}
+        </h5>
+        <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker3.about}</h6>
+        <span class='line2'></span>
+    </div>
+   </div>
+    </div>               
+</div>
+`;
 }
--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-function openMe() {
-  document.getElementById('navbar').style.height = '100%';
-}
-function closeMe() {
-  document.getElementById('navbar').style.height = '0%';
-}
+const openMe = document.querySelector('.menu')
+const closeMe = document.querySelectorAll('.close')
+const menuMobile = document.querySelector('#navbar')
 
-function hideShow() {
- const hiddenElement = document.querySelectorAll('.hide-cards');
-  hiddenElement.forEach((item) => {
+openMe.addEventListener('click', () => {
+  menuMobile.style.height = '100%'
+})
+
+closeMe.forEach(element => {
+  element.addEventListener('click', () => {
+    menuMobile.style.height = '0%'
+  });
+});
+
+const hiddenElement = document.querySelectorAll('.hide-cards')
+const hideBtn = document.querySelector('.hide-btn a')
+
+hideBtn.addEventListener('click', () => {
+
+
+   hiddenElement.forEach((item) => {
     if (item.style.visibility === 'hidden') {
       item.style.visibility = 'visible';
       item.style.height = '100%';
@@ -18,4 +29,5 @@ function hideShow() {
       document.querySelector('.hide-btn a').textContent = 'MORE';
     }
   });
-}
+});
+
