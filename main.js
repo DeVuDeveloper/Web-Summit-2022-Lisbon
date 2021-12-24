3c3c6abd6164268305949d1be326004a32203ebc
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-24 20:12:39 +0100

    errors linter

--- main.js
+++ main.js
@@ -3,31 +3,28 @@ const closeMe = document.querySelectorAll('.close')
 const menuMobile = document.querySelector('#navbar')
 
 openMe.addEventListener('click', () => {
-  menuMobile.style.height = '100%'
+    menuMobile.style.height = '100%'
 })
 
 closeMe.forEach(element => {
-  element.addEventListener('click', () => {
-    menuMobile.style.height = '0%'
-  });
-});
+    element.addEventListener('click', () => {
+        menuMobile.style.height = '0%'
+    })
+})
 
 const hiddenElement = document.querySelectorAll('.hide-cards')
 const hideBtn = document.querySelector('.hide-btn a')
 
 hideBtn.addEventListener('click', () => {
-
-
-   hiddenElement.forEach((item) => {
-    if (item.style.visibility === 'hidden') {
-      item.style.visibility = 'visible';
-      item.style.height = '100%';
-      document.querySelector('.hide-btn a').textContent = 'LESS';
-    } else {
-      item.style.visibility = 'hidden';
-      item.style.height = '0';
-      document.querySelector('.hide-btn a').textContent = 'MORE';
-    }
-  });
-});
-
+    hiddenElement.forEach(item => {
+        if (item.style.visibility === 'hidden') {
+            item.style.visibility = 'visible'
+            item.style.height = '100%'
+            document.querySelector('.hide-btn a').textContent = 'LESS'
+        } else {
+            item.style.visibility = 'hidden'
+            item.style.height = '0'
+            document.querySelector('.hide-btn a').textContent = 'MORE'
+        }
+    })
+})
